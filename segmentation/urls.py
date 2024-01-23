32a6e0a08e636def31e3bd66331a902eb60256f7
C4 Patino <c4patino@gmail.com>
2024-01-23 14:11:32 -0600

    Refactor segmentation and analysis views

--- segmentation/urls.py
+++ segmentation/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('api/analysis/', views.AnalysisView.as_view(), name='analysis'),
     path('api/segmentation/', views.SegmentationView.as_view(), name='segmentation'),
-    path('api/segmentation/labelme/', views.SegmentationLabelmeView.as_view(), name='segmentation-analysis'),
+    path('api/segmentation/labelme/', views.SegmentationLabelmeView.as_view(), name='segmentation_labelme'),
 ]
--- segmentation/views.py
+++ segmentation/views.py
@@ -50,9 +50,33 @@ class SegmentationView(GenericAPIView):
 class SegmentationLabelmeView(GenericAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = SegmentationSerializer
-    name = 'segmentation'
+    name = 'segmentation_labelme'
 
     def post(self, request):
+        """
+        Process a POST request to perform image segmentation.
+
+        This method takes in an image and a threshold value from the request data.
+        It performs image segmentation using the provided threshold and returns the segmented image data in JSON format.
+
+        Parameters:
+        - request: The HTTP request object containing the image and threshold data.
+
+        Returns:
+        - HttpResponse: The segmented image data in JSON format.
+
+        Raises:
+        - status.HTTP_400_BAD_REQUEST: If the request data is invalid.
+
+        Example Usage:
+        ```
+        POST /segmentation
+        {
+            "image": <image_data>,
+            "threshold": 0.5
+        }
+        ```
+        """
         serializer = self.get_serializer(data=request.data)
 
         if not serializer.is_valid():
@@ -79,18 +103,67 @@ class SegmentationLabelmeView(GenericAPIView):
 
 
 class AnalysisView(GenericAPIView):
-    """
-    Endpoint for performing analysis on an image.
-
-    This view accepts a POST request with an image and a scaling factor.
-    It performs various analysis operations on the image and returns the results.
-    """
-
     permission_classes = [permissions.AllowAny]
     serializer_class = AnalysisSerializer
     name = 'analysis'
 
+    """
+    API endpoint for performing analysis on an image.
+
+    This view accepts a POST request with an image and a scaling factor.
+    It performs various root analysis calculations on the image and returns the results.
+
+    Parameters:
+    - image: The image file to be analyzed.
+    - scaling_factor: The scaling factor to convert pixel measurements to real-world measurements.
+
+    Returns:
+    A JSON response containing the following analysis results:
+    - root_count: The number of roots detected in the image.
+    - root_diameter: The average diameter of the roots in the image.
+    - total_root_area: The total area occupied by the roots in the image.
+    - total_root_length: The total length of the roots in the image.
+    - total_root_volume: The total volume of the roots in the image.
+
+    Example usage:
+    ```
+    POST /analysis/
+    {
+        "image": <image_file>,
+        "scaling_factor": 0.1
+    }
+    ```
+
+    Example response:
+    ```
+    {
+        "root_count": 10,
+        "root_diameter": 0.5,
+        "total_root_area": 100,
+        "total_root_length": 50,
+        "total_root_volume": 25
+    }
+    ```
+    """
+
     def post(self, request):
+        """
+        API endpoint for processing a POST request.
+
+        This endpoint receives an image and a scaling factor, performs root analysis on the image,
+        and returns the results in JSON format.
+
+        Parameters:
+        - request: The HTTP request object containing the image and scaling factor.
+
+        Returns:
+        - A JSON response containing the following root analysis results:
+            - root_count: The number of roots detected in the image.
+            - root_diameter: The average diameter of the roots in the image.
+            - total_root_area: The total area covered by the roots in the image.
+            - total_root_length: The total length of the roots in the image.
+            - total_root_volume: The total volume of the roots in the image.
+        """
         serializer = self.get_serializer(data=request.data)
 
         if not serializer.is_valid():
