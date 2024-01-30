5906238a4c686cf698d07f718ad4d03ef1456702
C4 Patino <c4patino@gmail.com>
2024-01-30 16:57:35 -0600

    Add LabelMeSerializer to segmentation serializers.py

--- segmentation/apps.py
+++ segmentation/apps.py
@@ -2,9 +2,10 @@ from django.apps import AppConfig
 
 import torch
 
+
 class SegmentationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'segmentation'
 
-    model = torch.load('unet.pth')
+    model = torch.load('models/unet.pth')
     model.eval()
