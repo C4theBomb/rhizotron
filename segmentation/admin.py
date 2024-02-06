1d7680b9c877d4a88fb0379122519c90311aceb9
C4 Patino <c4patino@gmail.com>
2024-02-06 15:23:03 -0600

    Add model registrations to segmentation admin.py

--- segmentation/admin.py
+++ segmentation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from segmentation.models import Dataset, Image, Prediction
+
 # Register your models here.
+admin.site.register(Dataset)
+admin.site.register(Image)
+admin.site.register(Prediction)
