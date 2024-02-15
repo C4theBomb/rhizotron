ed54b5abf42f35083913940b2e2ed63770126b24
C4 Patino <c4patino@gmail.com>
2024-02-14 20:02:50 -0600

    Refactor BulkNestedRouter to use NestedMixin and SimpleRouter

--- segmentation/routers.py
+++ segmentation/routers.py
@@ -1,14 +1,17 @@
-from rest_framework_nested.routers import NestedSimpleRouter
+from rest_framework_nested.routers import NestedMixin, SimpleRouter
 from rest_framework.routers import Route, SimpleRouter
 
 
-class BulkNestedRouter(NestedSimpleRouter):
-    routes = [
-        Route(
-            url=r'^{prefix}{trailing_slash}$',
-            mapping={'get': 'list', 'post': 'create', 'delete': 'bulk_destroy'},
-            name='{basename}-images',
-            detail=False,
-            initkwargs={'suffix': ''}
-        )
-    ] + NestedSimpleRouter.routes + SimpleRouter.routes
+class BulkNestedRouter(NestedMixin, SimpleRouter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.routes += [
+            Route(
+                url=r'^{prefix}{trailing_slash}$',
+                mapping={'get': 'list', 'post': 'create', 'delete': 'bulk_destroy'},
+                name='{basename}-images',
+                detail=False,
+                initkwargs={'suffix': ''}
+            )
+        ]
