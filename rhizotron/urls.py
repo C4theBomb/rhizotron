3da654188d90fe8ca3826b4d24b2dd9ab968bff6
C4 Patino <c4patino@gmail.com>
2024-03-14 16:54:40 -0500

    Add static files to .gitignore and update urlpatterns

--- rhizotron/urls.py
+++ rhizotron/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     path('api/schema/redoc/', SpectacularRedocView.as_view(), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
