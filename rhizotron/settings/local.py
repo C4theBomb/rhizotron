8fc277366e70bf4a0183bc88ecc96927dd70e4ad
C4 Patino <c4patino@gmail.com>
2024-03-28 13:08:03 -0500

    Widlcard allow in ALLOWED_HOSTS for dev environments

--- rhizotron/settings/local.py
+++ rhizotron/settings/local.py
@@ -4,11 +4,7 @@ SECRET_KEY = 'django-insecure-k49qkhm&vo1ds7b3p%+_z6bt)!#aind%3x#-$@e8sq&%2kr8mc
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '10.17.1.163'
-]
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
